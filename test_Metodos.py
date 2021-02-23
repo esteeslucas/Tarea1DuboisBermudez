2e0908194195d84db0b3743577da3d7fbaf494c9
esteeslucas <50120421+esteeslucas@users.noreply.github.com>
2021-02-22 18:17:10 -0600

    Caso 2
    Se añade el caso 2 al archivo de testing para pytest

--- test_Metodos.py
+++ test_Metodos.py
@@ -5,3 +5,14 @@ def test_caso1():
     for i in range(len(letras)):
         resultado = Metodos.check_char(letras[i])
         assert resultado == 0
+    for i in range(len(letras)):
+        resultado = Metodos.check_char(letras[i].upper())
+        assert resultado == 0
+def test_caso2():
+    letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
+    for i in range(len(letras)):
+        resultado = Metodos.caps_switch(letras[i])
+        assert resultado == letras[i].upper()
+    for i in range(len(letras)):
+        resultado = Metodos.caps_switch(letras[i].upper())
+        assert resultado == letras[i].lower()
