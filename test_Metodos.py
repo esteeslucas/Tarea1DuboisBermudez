e7d0f58f85e22955c648c837fffb8b28f1e8b0d4
esteeslucas <50120421+esteeslucas@users.noreply.github.com>
2021-02-22 18:39:21 -0600

    Se añaden tests
    Se añaden casos requeridos

--- test_Metodos.py
+++ test_Metodos.py
@@ -1,7 +1,6 @@
 import Metodos
-
+letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
 def test_caso1():
-    letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
     for i in range(len(letras)):
         resultado = Metodos.check_char(letras[i])
         assert resultado == 0
@@ -9,10 +8,19 @@ def test_caso1():
         resultado = Metodos.check_char(letras[i].upper())
         assert resultado == 0
 def test_caso2():
-    letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
     for i in range(len(letras)):
         resultado = Metodos.caps_switch(letras[i])
         assert resultado == letras[i].upper()
     for i in range(len(letras)):
         resultado = Metodos.caps_switch(letras[i].upper())
         assert resultado == letras[i].lower()
+def test_caso3():
+    resultado = Metodos.check_char("abcd")
+    assert resultado == "Error, se introdujo más de un caracter"
+def test_caso4():
+    resultado = Metodos.check_char("1")
+    assert resultado == ("Error, se introdujo un caracter que no pertenece al alfabeto")
+def test_caso5():
+    resultado = Metodos.check_char(123)
+    assert resultado == ("Error, se introdujo un caracter que no es de tipo string")
+    
