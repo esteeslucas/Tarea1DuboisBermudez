9287beab6f0955d7bbefb3c11fbdbef5f93d0e67
renzoBC20 <79342825+renzoBC20@users.noreply.github.com>
2021-03-02 22:33:50 -0600

    Add files via upload

---
+++ Codigosinerrores.py
@@ -0,0 +1,10 @@
+lista = [2, 6, 9, 5]
+total = 0
+for i in range(len(lista)):
+    total += lista[i]
+    print(total)
+#errores corregidos
+#libreria tkinter importada pero no usada
+#linea en blanco
+#no se utilizan espacios alrededor del operador en la definición de "total"
+#espacio en blanco antes del parentesis del print
